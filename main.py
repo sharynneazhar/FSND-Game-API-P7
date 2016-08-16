831fe70ef6f7238b4ca8af183c5fd3e8bcc8131e
Sharynne Azhar <sharynne@mobilgov.com>
2016-08-15 22:28:39 -0500

    refactor and add method to get a game and cancel a game

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class SendReminderEmail(webapp2.RequestHandler):
         users = User.query(User.email != None)
         for user in users:
             subject = 'This is a reminder!'
-            body = 'Hello {}, try out Guess A Number!'.format(user.name)
+            body = 'Hello {}, try out War!'.format(user.name)
             # This will send test emails, the arguments to send_mail are:
             # from, to, subject, body
             mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
