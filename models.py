9974637f19cb468cf4112b36503fb716a45219c5
Sharynne Azhar <sharynne@mobilgov.com>
2016-08-15 22:27:58 -0500

    refactor guess-a-number models to war game models

--- models.py
+++ models.py
@@ -14,8 +14,8 @@ class User(ndb.Model):
     email = ndb.StringProperty()
 
 
-class WarGame(ndb.Model):
-    """War Game object"""
+class Game(ndb.Model):
+    """Game object"""
     user = ndb.KeyProperty(required=True, kind='User')
     user_deck = ndb.StringProperty(repeated=True)
     bot_deck = ndb.StringProperty(repeated=True)
@@ -30,16 +30,16 @@ class WarGame(ndb.Model):
         deck1 = deck[0:26]
         deck2 = deck[26:52]
 
-        game = WarGame(user=user,
-                       user_deck=deck1,
-                       bot_deck=deck2,
-                       game_over=False)
+        game = Game(user=user,
+                    user_deck=deck1,
+                    bot_deck=deck2,
+                    game_over=False)
         game.put()
         return game
 
     def to_form(self, message):
-        """Returns a WarGameForm representation of the WarGame"""
-        form = WarGameForm()
+        """Returns the response data of the game object"""
+        form = GameResource()
         form.urlsafe_key = self.key.urlsafe()
         form.user_name = self.user.get().name
         form.user_deck = self.user_deck
@@ -49,111 +49,26 @@ class WarGame(ndb.Model):
         return form
 
 
-class WarGameForm(messages.Message):
+class GenericMessage(messages.Message):
+    """StringMessage-- outbound (single) string message"""
+    message = messages.StringField(1, required=True)
+
+
+class UserResource(messages.Message):
+    user_name = messages.StringField(1, required=True)
+    email = messages.StringField(2)
+
+
+class GameResource(messages.Message):
     """Game form for outbound api response data"""
     urlsafe_key = messages.StringField(1, required=True)
     user_name = messages.StringField(2, required=True)
     user_deck = messages.StringField(3, repeated=True)
-    bot_deck= messages.StringField(4, repeated=True)
+    bot_deck = messages.StringField(4, repeated=True)
     message = messages.StringField(5, required=True)
     game_over = messages.BooleanField(6, required=True)
 
 
-class NewWarGameForm(messages.Message):
+class NewGameResource(messages.Message):
     """Used to create a new game"""
     user_name = messages.StringField(1, required=True)
-
-# -----------------------------------------------------------------------------
-
-class Game(ndb.Model):
-    """Game object"""
-    target = ndb.IntegerProperty(required=True)
-    attempts_allowed = ndb.IntegerProperty(required=True)
-    attempts_remaining = ndb.IntegerProperty(required=True, default=5)
-    game_over = ndb.BooleanProperty(required=True, default=False)
-    user = ndb.KeyProperty(required=True, kind='User')
-
-    @classmethod
-    def new_game(cls, user, min, max, attempts):
-        """Creates and returns a new game"""
-        if max < min:
-            raise ValueError('Maximum must be greater than minimum')
-        game = Game(user=user,
-                    target=random.choice(range(1, max + 1)),
-                    attempts_allowed=attempts,
-                    attempts_remaining=attempts,
-                    game_over=False)
-        game.put()
-        return game
-
-    def to_form(self, message):
-        """Returns a GameForm representation of the Game"""
-        form = GameForm()
-        form.urlsafe_key = self.key.urlsafe()
-        form.user_name = self.user.get().name
-        form.attempts_remaining = self.attempts_remaining
-        form.game_over = self.game_over
-        form.message = message
-        return form
-
-    def end_game(self, won=False):
-        """Ends the game - if won is True, the player won. - if won is False,
-        the player lost."""
-        self.game_over = True
-        self.put()
-        # Add the game to the score 'board'
-        score = Score(user=self.user, date=date.today(), won=won,
-                      guesses=self.attempts_allowed - self.attempts_remaining)
-        score.put()
-
-
-class Score(ndb.Model):
-    """Score object"""
-    user = ndb.KeyProperty(required=True, kind='User')
-    date = ndb.DateProperty(required=True)
-    won = ndb.BooleanProperty(required=True)
-    guesses = ndb.IntegerProperty(required=True)
-
-    def to_form(self):
-        return ScoreForm(user_name=self.user.get().name, won=self.won,
-                         date=str(self.date), guesses=self.guesses)
-
-
-class GameForm(messages.Message):
-    """GameForm for outbound game state information"""
-    urlsafe_key = messages.StringField(1, required=True)
-    attempts_remaining = messages.IntegerField(2, required=True)
-    game_over = messages.BooleanField(3, required=True)
-    message = messages.StringField(4, required=True)
-    user_name = messages.StringField(5, required=True)
-
-
-class NewGameForm(messages.Message):
-    """Used to create a new game"""
-    user_name = messages.StringField(1, required=True)
-    min = messages.IntegerField(2, default=1)
-    max = messages.IntegerField(3, default=10)
-    attempts = messages.IntegerField(4, default=5)
-
-
-class MakeMoveForm(messages.Message):
-    """Used to make a move in an existing game"""
-    guess = messages.IntegerField(1, required=True)
-
-
-class ScoreForm(messages.Message):
-    """ScoreForm for outbound Score information"""
-    user_name = messages.StringField(1, required=True)
-    date = messages.StringField(2, required=True)
-    won = messages.BooleanField(3, required=True)
-    guesses = messages.IntegerField(4, required=True)
-
-
-class ScoreForms(messages.Message):
-    """Return multiple ScoreForms"""
-    items = messages.MessageField(ScoreForm, 1, repeated=True)
-
-
-class StringMessage(messages.Message):
-    """StringMessage-- outbound (single) string message"""
-    message = messages.StringField(1, required=True)
